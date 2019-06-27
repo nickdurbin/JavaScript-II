0fa20a932a67e24de0c6cd80b6d934cef29958a8
nickdurbin <nick.durbin@gmail.com>
2019-06-26 21:42:23 -0400

    Got most of the normal work complete.

--- assignments/closure.js
+++ assignments/closure.js
@@ -32,57 +32,41 @@ console.log(newCounter());
 
 
 // ==== Challenge 3: Create a counter function with an object that can increment and decrement ====
-<<<<<<< HEAD
-// Return an object that has two methods called `increment` and `decrement`.
-// `increment` should increment a counter variable in closure scope and return it.
-// `decrement` should decrement the counter variable and return it.
-const counterFactory = () => {
- 
-  let num = 0;
-  const increment = () => {
-    return function() {
-      return ++num;
-=======
   // Return an object that has two methods called `increment` and `decrement`.
   // `increment` should increment a counter variable in closure scope and return it.
   // `decrement` should decrement the counter variable and return it.
+// const counterFactory = () => {
+
+//   let counter = 0;
+
+//   const increment = () => {
+  
+//     return function() {
+//       return ++counter;
+//     }
+//   };
+
+//   const decrement = () => {
+  
+//     return function() {
+//       return --counter;
+//     }
+//   };
+
+//   const addCounter = increment();
+//   const minusCounter = decrement();
+
+//   const answer = [addCounter(), addCounter(), minusCounter()];
+//   return answer;
+// };
+
+// console.log(counterFactory());
+
+let  num = 0;
 const counterFactory = () => {
-
-  let counter = 0;
-
-  const increment = () => {
-  
-    return function() {
-      return ++counter;
->>>>>>> 23be6e032e0d3c4a58df4f25bd3d6d134a3d2076
-    }
-  };
-
-  const decrement = () => {
-<<<<<<< HEAD
-    
-    return function() {
-      return --num;
-=======
-  
-    return function() {
-      return --counter;
->>>>>>> 23be6e032e0d3c4a58df4f25bd3d6d134a3d2076
-    }
-  };
-
-  const addCounter = increment();
-  const minusCounter = decrement();
-
-  const answer = [addCounter(), addCounter(), minusCounter()];
-  return answer;
+  return {
+    increment: function() { return ++num },
+    decrement: function() { return --num}
+  }  
 };
 
-<<<<<<< HEAD
-console.log(counterFactory());
-
-  
-  
-=======
-console.log(counterFactory());
->>>>>>> 23be6e032e0d3c4a58df4f25bd3d6d134a3d2076
