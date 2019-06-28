9d70720ba329cd12f9a1ad71bab36c144c0e0330
nickdurbin <nick.durbin@gmail.com>
2019-06-28 18:05:25 -0400

    Trying to pull my other work environment down to the desktop.

--- assignments/callbacks.js
+++ assignments/callbacks.js
@@ -57,13 +57,13 @@ multiplyNums(4, 7, (multiply) => {
   console.log(multiply);
 });
 
-// function contains(item, list, cb) {
-//   // contains checks if an item is present inside of the given array/list.
-//   // Pass true to the callback if it is, otherwise pass false.
-//   items.filter(item, list => {
-//     console.log(list);
-//   });
-// }
+function contains(item, list, cb) {
+  // contains checks if an item is present inside of the given array/list.
+  // Pass true to the callback if it is, otherwise pass false.
+  items.filter(item, list => {
+    console.log(list);
+  });
+}
 
 /* STRETCH PROBLEM */
 
