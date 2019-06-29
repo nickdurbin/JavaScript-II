67e0c0b1ee376bde23e9592ba961646598ef9c3d
nickdurbin <nick.durbin@gmail.com>
2019-06-28 20:26:38 -0400

    Completed the stretch using the ...Set method.

--- assignments/callbacks.js
+++ assignments/callbacks.js
@@ -1,7 +1,7 @@
 // Create a higher order function and invoke the callback function to test your work. You have been provided an example of a problem and a solution to see how this works with our items array.  Study both the problem and the solution to figure out the rest of the problems.
 
 const items = ['Pencil', 'Notebook', 'yo-yo', 'Gum'];
-
+const itemsDouble = ['Pencil', 'Notebook', 'yo-yo', 'Gum', 'Pencil', 'Notebook', 'yo-yo', 'Gum', 'Apple']
 
 /* 
 
@@ -25,7 +25,7 @@ const items = ['Pencil', 'Notebook', 'yo-yo', 'Gum'];
 
 */
 
-
+// getLength passes the length of the array to the callback
 function getLength(arr, cb) {
   return cb(arr.length);
 }
@@ -34,6 +34,7 @@ getLength(items, function(length) {
   console.log(length)
 });
 
+// Calls the last item in the array and passes to the callback
 function last(arr, cb) {
   return cb(arr[arr.length - 1]);
 }
@@ -42,6 +43,7 @@ last(items, function(lastItem) {
   console.log(lastItem);
 });
 
+// Sum of two numbers and return them in a callback
 function sumNums(x, y, cb) {
   return cb(x + y);
 }
@@ -50,6 +52,7 @@ sumNums(2, 3, (sum) => {
   console.log(sum);
 });
 
+// Product of two numbers and return in a callback
 function multiplyNums(x, y, cb) {
   return cb(x * y);
 }
@@ -58,23 +61,12 @@ multiplyNums(4, 7, (multiply) => {
   console.log(multiply);
 });
 
-<<<<<<< HEAD
-function contains(item, list, cb) {
-  // contains checks if an item is present inside of the given array/list.
-  // Pass true to the callback if it is, otherwise pass false.
-  items.filter(item, list => {
-    console.log(list);
-  });
-}
-
-/* STRETCH PROBLEM */
-=======
 // contains checks if an item is present inside of the given array/list.
 // Pass true to the callback if it is, otherwise pass false.
 function contains(item, list, cb) {
-  list.includes(item) ? cb(true) : cb(false);
+  // return list.includes(item) ? cb(true) : cb(false);
+  return cb(list.includes(item));
 };
->>>>>>> a1dc302a67daa964a9daf195f5b9d04d7ada49cf
 
 let value = exists => {
   console.log(exists);
@@ -91,8 +83,12 @@ contains('Notebook', items, value);
 //   // removeDuplicates removes all duplicate values from the given array.
 //   // Pass the duplicate free array to the callback function.
 //   // Do not mutate the original array.
-// function removeDuplicates(array, cb) {
-//   array.filter(duplicate => duplicate === cb.items);
-// }
+function removeDuplicates(array, cb) {
+  return cb(array);
+}
 
-// console.log(removeDuplicates(items, duplicate));
+let dubs = removeDuplicates(itemsDouble, function(arr) {
+  return [...new Set(arr)];
+})
+
+console.log(dubs);
